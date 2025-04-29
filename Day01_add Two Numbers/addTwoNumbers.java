877c48bf3d41cb5b16e55959c8a4a10644cf854c
shibi22 <shibi73201@gmail.com>
2025-04-29 21:52:11 +0530

    Day 01: Solved Two Sum

---
+++ Day01_add Two Numbers/addTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class  addTwoNumbers{
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 1, 2, 4, 8};
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        System.out.println(" Max: " + max);
+        System.out.println(" Min: " + min);
+    }
+}
