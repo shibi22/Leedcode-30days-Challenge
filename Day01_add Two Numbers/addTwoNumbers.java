6803aa59b2e02c49b5c54df23f6187506b5c9f35
shibi22 <shibi73201@gmail.com>
2025-04-29 21:59:55 +0530

    Day 01: Solved addTwoNumbers

--- Day01_add Two Numbers/addTwoNumbers.java
+++ Day01_add Two Numbers/addTwoNumbers.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
-class  addTwoNumbers{
+class  addTwoNumbers { 
     public static void main(String[] args) {
         int[] arr = {3, 5, 1, 2, 4, 8};
 
-        int max = Arrays.stream(arr).max().getAsInt();
-        int min = Arrays.stream(arr).min().getAsInt();
+         int max = Arrays.stream(arr).max().getAsInt();
+         int min = Arrays.stream(arr).min().getAsInt();
 
-        System.out.println(" Max: " + max);
-        System.out.println(" Min: " + min);
+        System.out.println(" Max: "  + max);
+        System.out.println(" Min: "  + min);
     }
 }
