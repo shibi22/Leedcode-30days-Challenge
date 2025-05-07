bda439968f0fc08f6d93a7b323d907d2ef4f4645
shibi22 <shibi73201@gmail.com>
2025-05-07 22:24:45 +0530

    Day 10: solved count-symmetric-integers

---
+++ Day10-count-symmetric-integers/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            String s = Integer.toString(i);
+            int len = s.length();
+            if (len % 2 != 0) continue; // must have even number of digits
+
+            int half = len / 2;
+            int sum1 = 0, sum2 = 0;
+            for (int j = 0; j < half; j++) {
+                sum1 += s.charAt(j) - '0';
+            }
+            for (int j = half; j < len; j++) {
+                sum2 += s.charAt(j) - '0';
+            }
+
+            if (sum1 == sum2) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
